b8c28099992b1a13f7f00260d98a456f687718c0
Andrés Cardona <andres@cardonacoder.com>
2021-05-21 12:32:31 -0500

    Add dif. logo to Log and Reg

--- docs/index.js
+++ docs/index.js
@@ -2,6 +2,8 @@
 let reg = document.getElementById("reg");
 let ing = document.getElementById("ing");
 let button = document.getElementById("button");
+let emailBox = document.getElementById("email");
+let logRegPage = document.getElementById("logRegPage");
 
 function regFX(){ 
 
@@ -10,6 +12,8 @@ function regFX(){
     ing.classList.add("inactive");
     ing.classList.add("underlineHover");
     button.value ="Registrarse";
+    emailBox.style.display = "inline";
+    logRegPage.src = "./media/1.RegLandigPage.png";
 }
 
 function ingFX(){ 
@@ -19,6 +23,8 @@ function ingFX(){
     reg.classList.add("inactive");
     reg.classList.add("underlineHover");
     button.value ="ingresar";
+    emailBox.style.display = "none";
+    logRegPage.src = "./media/1.LogLandigPage.png";
 }
 
 
